096d386c003c1253ebb2bbbc2fdaa0114b95e182
Victor Higaki <victorhigaki@outlook.com>
2022-11-02 15:14:53 -0300

    @mouseover @mouseleave @dblclick @mousemove

--- app.js
+++ app.js
@@ -1,17 +1,29 @@
 const app = Vue.createApp({
     data() {
-      return {
-        showBooks: true,
-        title: 'The Way of Kings',
-        author: 'Brandon Sanderson',
-        age: 45
-      }
+        return {
+            showBooks: true,
+            title: 'The Way of Kings',
+            author: 'Brandon Sanderson',
+            age: 45,
+            x: 0,
+            y: 0,
+        }
     },
     methods: {
-      toggleShowBooks() {
-        this.showBooks =  !this.showBooks
-      }
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
+        },
+        handleEvent(e, data) {
+            console.log(e, e.type)
+            if (data) {
+                console.log(data)
+            }
+        },
+        handleMousemove(e) {
+            this.x = e.offsetX
+            this.y = e.offsetY
+        }
     }
-  })
-  
-  app.mount('#app')
+})
+
+app.mount('#app')
