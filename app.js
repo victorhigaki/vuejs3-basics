90f7ca92fbb90d076c9e8dd111d778f790041a71
Victor Higaki <victorhigaki@outlook.com>
2022-11-02 15:05:59 -0300

    conditional v-if v-else v-show

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
 const app = Vue.createApp({
     data() {
-        return {
-            title: 'The Way of Kings',
-            author: 'Brandon Sanderson',
-            age: 45
-        }
+      return {
+        showBooks: true,
+        title: 'The Way of Kings',
+        author: 'Brandon Sanderson',
+        age: 45
+      }
     },
     methods: {
-        // changeTitle() {
-        //     this.title = 'Words of Radiance'
-        // },
-        changeTitle(title) {
-            this.title = title
-        }
+      toggleShowBooks() {
+        this.showBooks =  !this.showBooks
+      }
     }
-})
-
-app.mount('#app')
+  })
+  
+  app.mount('#app')
